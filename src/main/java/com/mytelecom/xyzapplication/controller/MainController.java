5875fae2daf3e9b506cb6a2eea9e6a5f373eb1a6
amit mishra <kumar.amit.mishra@gmail.com>
2021-11-30 22:40:24 +1100

    commit

--- src/main/java/com/mytelecom/xyzapplication/controller/MainController.java
+++ src/main/java/com/mytelecom/xyzapplication/controller/MainController.java
@@ -17,32 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api1")
 public class MainController {
 
-    Logger logger=LoggerFactory.getLogger(MainController.class);
+    Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @Autowired
     private PhoneService phoneService;
 
-    @GetMapping(value ="/allPhoneNumbers")
+    @GetMapping(value = "/allPhoneNumbers")
     @ApiOperation("Get all the phone numbers")
-    public List<Phone> getAllPhoneNumbers(){
+    public List<Phone> getAllPhoneNumbers() {
         logger.info("Entering getAllPhoneNumbers");
         return phoneService.getAllPhoneNumbers();
     }
 
-    @GetMapping(value ="/allPhoneNumbersByCustId/{customerid}")
+    @GetMapping(value = "/allPhoneNumbersByCustId/{customerid}")
     @ApiOperation("Get all the phone numbers based on Customer Id")
-    public List<Phone> getAllPhoneNumbersByCustomerId(@PathVariable("customerid") Long customerId) throws CustomerNotFound {
+    public List<Phone> getAllPhoneNumbersByCustomerId(@PathVariable("customerid") Long customerId)
+            throws CustomerNotFound {
         logger.info("Entering getAllPhoneNumbersByCustomerId");
         return phoneService.getAllPhoneNumbersByCustomerId(customerId);
     }
 
-    @GetMapping(value ="/activatePhoneNumber/{phoneNum}")
-    public String activatePhoneNumber(@PathVariable("phoneNum")Long phoneNumber) throws PhoneNumNotFound {
+    @GetMapping(value = "/activatePhoneNumber/{phoneNum}")
+    public String activatePhoneNumber(@PathVariable("phoneNum") Long phoneNumber) throws PhoneNumNotFound {
         logger.info("Entering activatePhoneNumber");
         phoneService.activatePhone(phoneNumber);
-         return "Phone number activated";
+        return "Phone number activated";
     }
 }
